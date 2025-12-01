3915f85379f805d6b5d3a94323a38f067f3812f5
Sagar Tarak <114102151+Sagar-Tarak@users.noreply.github.com>
2025-12-01 18:05:05 +0530

    Add snacks.nvim plugin configuration

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        enabled = true, -- ENABLE FULL EXPLORER
+        replace_netrw = true,
+        follow_current_file = true,
+        view = {
+          width = 35,
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
